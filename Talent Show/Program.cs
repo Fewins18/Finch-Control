6d880ec0728ab6aaac9a5a15e7394ca492e62c24
Dylon <dylonfewins@gmail.com>
2020-10-04 01:14:59 -0400

    Add project files.

---
+++ Talent Show/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Talent_Show
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
